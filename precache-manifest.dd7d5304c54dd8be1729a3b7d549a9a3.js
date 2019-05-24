75547af4804ab0dc074b8ce50378f4b6c1e39344
With0utFace <mardarov.artiom@gmail.com>
2019-05-24 09:48:06 +0300

    Updates

---
+++ precache-manifest.dd7d5304c54dd8be1729a3b7d549a9a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab25a65b84aa6c4d86b8b1e1664cb06",
+    "url": "/burger-builder/static/media/logo.fab25a65.jpg"
+  },
+  {
+    "revision": "5e75f81a590432affc55a767c216b12c",
+    "url": "/burger-builder/static/media/burger-sprite.5e75f81a.png"
+  },
+  {
+    "revision": "6168d9880ae57e7f6b00",
+    "url": "/burger-builder/static/js/runtime~main.ad75294f.js"
+  },
+  {
+    "revision": "2947eb4525addcc03ce4",
+    "url": "/burger-builder/static/js/main.735509cf.chunk.js"
+  },
+  {
+    "revision": "6f14c7c2e59669ad3b4d",
+    "url": "/burger-builder/static/js/2.fdb0033c.chunk.js"
+  },
+  {
+    "revision": "2947eb4525addcc03ce4",
+    "url": "/burger-builder/static/css/main.441382db.chunk.css"
+  },
+  {
+    "revision": "533e4b4bcc1181772c6f172fbd2e280c",
+    "url": "/burger-builder/index.html"
+  }
+];
