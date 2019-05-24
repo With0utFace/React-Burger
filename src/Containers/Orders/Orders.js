3ac04d5441ae89ac2d04e28f39b0a426d44ed5a3
With0utFace <mardarov.artiom@gmail.com>
2019-05-24 11:50:48 +0300

    Changed few Styles, Added github-pages

--- src/Containers/Orders/Orders.js
+++ src/Containers/Orders/Orders.js
@@ -1,95 +1,99 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { fetchOrdersToState, removeOrder, getLocalToken } from "../../Store/actions";
-import { Redirect } from "react-router-dom";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+    fetchOrdersToState,
+    removeOrder,
+    getLocalToken
+} from '../../Store/actions';
+import { Redirect } from 'react-router-dom';
 
-import Loader from "../../Components/UI/Loader/Loader";
-import ErrorHanlder from "../../Components/UI/ErrorHandler/ErrorHandler";
-import Button from "../../Components/UI/Button/Button";
+import Loader from '../../Components/UI/Loader/Loader';
+import ErrorHanlder from '../../Components/UI/ErrorHandler/ErrorHandler';
+import Button from '../../Components/UI/Button/Button';
 
-import Order from "../../Components/OneOrder/OneOrder";
+import Order from '../../Components/OneOrder/OneOrder';
 
-import "../../Styles/Components/Orders.scss";
+import '../../Styles/Components/Orders.scss';
 
 class Orders extends Component {
-  state = {
-    activeError: false
-  };
+    state = {
+        activeError: false
+    };
 
-  componentDidMount() {
-    if (this.props.token) {
-      this.props.getOrders(this.props.orders);
-    }
-  }
-
-  removeOrderHandler = id => {
-    this.props.removeOrder(id, this.props.token);
-  };
-
-  pushToMain = () => {
-    this.props.history.push("/");
-  };
-
-  render() {
-    const { orders, loading, token } = this.props;
-
-    let userOrders = null;
-
-    if (!token) {
-      return <Redirect to="/" />;
+    componentDidMount() {
+        if (this.props.token) {
+            this.props.getOrders(this.props.orders);
+        }
     }
 
-    if (loading) {
-      return (userOrders = <Loader />);
+    removeOrderHandler = id => {
+        this.props.removeOrder(id, this.props.token);
+    };
+
+    pushToMain = () => {
+        this.props.history.push('/');
+    };
+
+    render() {
+        const { orders, loading, token } = this.props;
+
+        let userOrders = null;
+
+        if (!token) {
+            return <Redirect to="/" />;
+        }
+
+        if (loading) {
+            return (userOrders = <Loader />);
+        }
+
+        if (!orders) {
+            return (userOrders = (
+                <ErrorHanlder className="no-items">
+                    <p>Looks like you have no orders yet</p>
+                    <Button type="primary" clicked={this.pushToMain}>
+                        go back and create them
+                    </Button>
+                </ErrorHanlder>
+            ));
+        }
+
+        userOrders = Object.keys(orders).map(order => {
+            return (
+                <div key={order} className="one-order">
+                    <div className="order-price">
+                        Burger Price: <strong>{orders[order].price}</strong>{' '}
+                    </div>
+                    <Order
+                        ingredients={orders[order].ingredients}
+                        clicked={() => this.removeOrderHandler(order)}
+                        orderRemoveError={() => {}}
+                    />
+                </div>
+            );
+        });
+
+        return <div className="orders">{userOrders}</div>;
     }
-
-    if (!orders) {
-      return (userOrders = (
-        <ErrorHanlder className="no-items">
-          <p>Looks like you have no orders yet</p>
-          <Button type="primary" clicked={this.pushToMain}>
-            go back and create them
-          </Button>
-        </ErrorHanlder>
-      ));
-    }
-
-    userOrders = Object.keys(orders).map(order => {
-      return (
-        <div key={order} className="one-order">
-          <div className="order-price">
-            Burger Price: <strong>{orders[order].price}</strong>{" "}
-          </div>
-          <Order
-            ingredients={orders[order].ingredients}
-            clicked={() => this.removeOrderHandler(order)}
-            orderRemoveError={() => {}}
-          />
-        </div>
-      );
-    });
-
-    return <div className="orders">{userOrders}</div>;
-  }
 }
 
 const mapStateToProps = state => {
-  return {
-    orders: state.orders.orders,
-    loading: state.orders.loading,
-    token: state.auth.token
-  };
+    return {
+        orders: state.orders.orders,
+        loading: state.orders.loading,
+        token: state.auth.token
+    };
 };
 
 const mapDispatchToProps = dispatch => {
-  return {
-    getOrders: () => dispatch(fetchOrdersToState()),
-    removeOrder: (id, token) => dispatch(removeOrder(id, token)),
-    getToken: () => dispatch(getLocalToken())
-  };
+    return {
+        getOrders: () => dispatch(fetchOrdersToState()),
+        removeOrder: (id, token) => dispatch(removeOrder(id, token)),
+        getToken: () => dispatch(getLocalToken())
+    };
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(Orders);
